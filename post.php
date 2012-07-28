090f3b01e7515e93cc9cc3ca97e3a12c9c2be70a
Severin Schols <severin@schols.de>
2012-07-28 12:22:55 +0200

    Template system, debug mode, and some cleanup.

--- post.php
+++ post.php
@@ -2,6 +2,9 @@
 	// Config
   $template_path = "./templates/";
   $email = "severin@schols.de";
+  
+  // Debug mode?!?
+  $debug = file_exists("DEBUG_ENABLED");
 	
 	// Get input information from POST request
 	if (!isset($_POST['body'])) {
@@ -11,18 +14,35 @@
 	$body = $_POST['body'];
 	$json = json_decode($body);
 	
+	// Sanitize inputs
+	if (!in_array($json->type, array("gt", "gte", "lt", "lte", "eq", "change", "frozen", "live"))) {
+			echo "Invalid type";
+			exit();
+	}
+	
 	// Read content from template
-	//...
+	$content = file_get_contents($template_path . $json->type);
 	
 	// Replace placeholders
-	//...
+	$replacements = array(
+	  "%FEED_TITLE%" => $json->environment->title,
+	  "%FEED_ID%" => $json->environment->id,
+	  "%FEED_URL%" => $json->environment->feed,
+		"%DATASTREAM_ID%" => $json->triggering_datastream->id,
+		"%DATASTREAM_TIMESTAMP%" => $json->triggering_datastream->at,
+		"%DATASTREAM_CURRENT_VALUE%" => $json->triggering_datastream->value->current_value
+		);
+	$content = str_replace(array_keys($replacements), array_values($replacements), $content);
 	
-	$subject = "Cosm Trigger - Frozen datastream";
-	$content = "Frozen datastream: " . $json->triggering_datastream->url;
+	$subject = "Cosm Trigger from feed \"{$json->environment->title}\"";
+	//$content = "Frozen datastream: " . $json->triggering_datastream->url;
 	
 	// Debug Output
-	echo "<html><body><h3>$subject</h3><p><i>To: $email</i></p><pre>$content</pre></body></html>";
+	if ($debug) {
+		echo "<html><body><h3>$subject</h3><p><i>To: $email</i></p><pre>$content</pre></body></html>";
+		exit();
+	}
 	
 	// Send eMail
-	//mail($email, $subject , $content);
+	mail($email, $subject , $content);
 ?>
